940b82c6a8e1488b0bfbb5840992b9fafc0e56a1
anusman1999 <42172727+anusman1999@users.noreply.github.com>
2018-08-16 14:28:24 +0000

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,25 @@
+// Library
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello" << endl;
+	// Declaring variables of all types
+	int value;              //Declaring a variable
+	int a,b;                // Declaring variables together also possible
+	double doubleVar = 1.2; // Defining the variables. 
+	bool boolVar = false;   // Defining a variable
+        char c;                 // Declearing a variable
+        float d=122141;         // Defining the variable
+        
+
+	
+	// Displaying the sizes of data types. 
+	cout << "Size of a = " << sizeof(a) << endl;
+	cout << "Size of doubleVar = " << sizeof(doubleVar) << endl;
+	cout << "Size of boolVar = " << sizeof(boolVar) << endl;
+	cout << "Size of c = " << sizeof(c) << endl;
+        cout << "Size of d = " <<sizeof(d) <<endl;
+        
+}
+
